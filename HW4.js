f6c5bef995c3fd05464f64d5fd9d767b6f61ef47
Mhmd.K <38146273+maxswag@users.noreply.github.com>
2024-12-11 14:58:11 +0200

    Update HW4.js

--- HW4.js
+++ HW4.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 function getOutPut(i, sentences) {
   /**
-   * @input1 index of text file
+   * @input1 index of txt file
    * @input2 array of lines of the text file
    *
    * @output string that contains 'i' lines if the text file has 'i' lines
@@ -13,9 +13,13 @@ function getOutPut(i, sentences) {
   let output1 = "";
 
   if (sentences.length < i)
+    //goes through the array of lines to concat all lines into output1 
     for (sentence of sentences) {
       output1 += `${sentence} \n`;
     }
+    
+//goes through the array of lines to concat "i" lines into output1 
+
   else for (let j = 0; j < i; j++) output1 += `${sentences[j]} \n`;
 
   return output1;
@@ -23,12 +27,13 @@ function getOutPut(i, sentences) {
 
 function GoThroughFiles() {
   /**
-   * Function that goes through all of the text files then returns a string containing 'n' sentences from each
+   * Function that goes through all of the text files and returns a string containing 'n' sentences from each
    * text file
    */
 
   let output2 = "";
 
+  //gets content of text files and splits them into an array according to new line separation
   for (let i = 1; i <= 10; i++) {
     let text = fs.readFileSync(`textfiles/${i}.txt`, "utf-8");
     let arr = text.split("\r\n");
